e4afbac9ece0aca401be6a335d3fdf31274efe7e
Daphney <daphneyalexandre@hotmail.com>
2024-05-15 09:52:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+
+
+// mostProfitableNeighborhood and companyCeo — Here we are getting more practice with declaring variables. Read the tests to see how you need to code these two variables to get the remaining tests passing.
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
